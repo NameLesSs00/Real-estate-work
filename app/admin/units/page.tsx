b82a8d43323e04b004d37e97bc2ebaaff6e39c73
Pola Samy <polasamy81311518@gmail.com>
2026-05-02 19:52:44 +0300

    testing please wokr

--- app/admin/units/page.tsx
+++ app/admin/units/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import AddUnitModal from '@/components/admin/AddUnitModal';
 import DeleteUnitModal from '@/components/admin/DeleteUnitModal';
