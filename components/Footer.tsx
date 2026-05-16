fd8fd6b3e0b50a4b4adeb67d7acf69bd91a5095f
Paul samy <polasamy81311518@gmail.com>
2026-05-16 12:20:52 +0300

    email Fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -72,7 +72,7 @@ const Footer = () => {
             </li>
             <li className="flex items-start gap-4">
               <Earth size={20} className="flex-shrink-0 mt-0.5" />
-              <a href="mailto:info@thegateestates.com" className="hover:text-white transition-colors break-all">info@thegateestates.com</a>
+              <a href="mailto:info@thegate-estates.com" className="hover:text-white transition-colors break-all">info@thegate-estates.com</a>
             </li>
             <li className="flex items-start gap-4">
               <MapPin size={20} className="flex-shrink-0 mt-0.5" />
