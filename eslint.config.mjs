5920ed7eda84722b1192c5dfd3f5dd18eb201d7b
pola samy <polasamy81311518@gmail.com>
2026-09-01 16:13:35 +0300

    Fix Windows server build command

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: [".next/**", "next-env.d.ts", "app.js", "server.js"],
+    ignores: [".next/**", "next-env.d.ts", "app.js", "server.js", "build.js"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
